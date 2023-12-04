fead99042c956940b4f8cc4e3a9cdb7dd82459f3
wjdtjsrms <9kjmse@naver.com>
2023-12-04 17:00:10 +0900

    <Update List> Jsg 1. Custom Lit 완성본 추가

--- Assets/_UnityStudy/6_Shader/MyLit.shader
+++ Assets/_UnityStudy/6_Shader/MyLit.shader
@@ -5,6 +5,7 @@ Shader "Jsg/MyLit"
 		[Header(Surface options)]
 		[MainTexture] _ColorMap("Color", 2D) = "white" {}
         [MainColor] _ColorTint("Tint", Color) = (1, 1, 1, 1)
+		_Smoothness("Smoothness", float) = 1
     }
     SubShader
     {
@@ -17,6 +18,17 @@ Shader "Jsg/MyLit"
 			Tags { "LightMode" = "UniversalForward" }
 		
 			HLSLPROGRAM 
+			
+			#define _SPECULAR_COLOR
+			
+			#if UNITY_VERSION >= 202120
+            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE
+#else
+            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
+#endif
+            #pragma multi_compile_fragment _ _SHADOWS_SOFT
+			
 			#pragma vertex Vertex
             #pragma fragment Fragment
 			
--- Assets/_UnityStudy/6_Shader/MyLitForwardLitPass.hlsl
+++ Assets/_UnityStudy/6_Shader/MyLitForwardLitPass.hlsl
@@ -3,6 +3,7 @@
 struct Attributes
 {
     float3 positionOS : POSITION;
+	float3 normalOS : NORMAL;
 	float2 uv : TEXCOORD0;
 };
 
@@ -10,6 +11,8 @@ struct Interpolators
 {
 	float4 positionCS : SV_POSITION;
 	float2 uv : TEXCOORD0;
+	float3 positionWS : TEXCOORD1;
+	float3 normalWS : TEXCOORD2;
 };
 
 TEXTURE2D(_ColorMap);
@@ -19,6 +22,7 @@ CBUFFER_START(UnityPerMaterial)
 
 	float4 _ColorTint;
 	float4 _ColorMap_ST;
+	float _Smoothness;
 	
 CBUFFER_END
 
@@ -27,9 +31,12 @@ Interpolators Vertex(Attributes input)
     Interpolators output;
     
 	VertexPositionInputs posInput = GetVertexPositionInputs(input.positionOS);
+	VertexNormalInputs normInputs = GetVertexNormalInputs(input.normalOS);
 	
 	output.positionCS = posInput.positionCS;
 	output.uv = TRANSFORM_TEX(input.uv, _ColorMap);
+	output.normalWS = normInputs.normalWS;
+	output.positionWS = posInput.positionWS;
 
 	return output;
 }
@@ -39,5 +46,21 @@ float4 Fragment(Interpolators input) : SV_TARGET
 	float2 uv = input.uv;
 	float4 colorSample = SAMPLE_TEXTURE2D(_ColorMap, sampler_ColorMap, uv);
 	
-	return colorSample * _ColorTint;
+	InputData lightingInput  = (InputData)0;
+	lightingInput.positionWS = input.positionWS;
+	lightingInput.normalWS = normalize(input.normalWS);
+	lightingInput.viewDirectionWS = GetWorldSpaceNormalizeViewDir(input.positionWS);
+	lightingInput.shadowCoord = TransformWorldToShadowCoord(input.positionWS);
+	
+	SurfaceData surfaceInput = (SurfaceData)0;
+	surfaceInput.albedo = colorSample.rgb * _ColorTint.rgb;
+	surfaceInput.alpha = colorSample.a * _ColorTint.a;
+	surfaceInput.specular = 1;
+	surfaceInput.smoothness = _Smoothness;
+	
+#if UNITY_VERSION >= 202120
+	return UniversalFragmentBlinnPhong(lightingInput, surfaceInput);
+#else
+	return UniversalFragmentBlinnPhong(lightingInput, surfaceInput.albedo, float4(surfaceInput.specular, 1), surfaceInput.smoothness, 0, surfaceInput.alpha);
+#endif
 }
