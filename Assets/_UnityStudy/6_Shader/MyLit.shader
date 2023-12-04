eaa9a3153dd8a12b12c769bf934d07027ef88c55
wjdtjsrms <9kjmse@naver.com>
2023-12-04 14:57:00 +0900

    <Update List> Jsg 1. Texture 추가

--- Assets/_UnityStudy/6_Shader/MyLit.shader
+++ Assets/_UnityStudy/6_Shader/MyLit.shader
@@ -2,7 +2,9 @@ Shader "Jsg/MyLit"
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}
+		[Header(Surface options)]
+		[MainTexture] _ColorMap("Color", 2D) = "white" {}
+        [MainColor] _ColorTint("Tint", Color) = (1, 1, 1, 1)
     }
     SubShader
     {
--- Assets/_UnityStudy/6_Shader/MyLitForwardLitPass.hlsl
+++ Assets/_UnityStudy/6_Shader/MyLitForwardLitPass.hlsl
@@ -3,12 +3,24 @@
 struct Attributes
 {
     float3 positionOS : POSITION;
+	float2 uv : TEXCOORD0;
 };
 
 struct Interpolators
 {
 	float4 positionCS : SV_POSITION;
+	float2 uv : TEXCOORD0;
 };
+
+TEXTURE2D(_ColorMap);
+SAMPLER(sampler_ColorMap);
+
+CBUFFER_START(UnityPerMaterial)
+
+	float4 _ColorTint;
+	float4 _ColorMap_ST;
+	
+CBUFFER_END
 
 Interpolators Vertex(Attributes input)
 {
@@ -17,11 +29,15 @@ Interpolators Vertex(Attributes input)
 	VertexPositionInputs posInput = GetVertexPositionInputs(input.positionOS);
 	
 	output.positionCS = posInput.positionCS;
+	output.uv = TRANSFORM_TEX(input.uv, _ColorMap);
 
 	return output;
 }
 
 float4 Fragment(Interpolators input) : SV_TARGET
 {
-	return float4(1, 1, 1, 1);
+	float2 uv = input.uv;
+	float4 colorSample = SAMPLE_TEXTURE2D(_ColorMap, sampler_ColorMap, uv);
+	
+	return colorSample * _ColorTint;
 }
