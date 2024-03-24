70f2b9707b68e8cc2eb1326d867aef3e08855676
wjdtjsrms <9kjmse@naver.com>
2024-03-25 08:15:15 +0900

    <Update List> 1. R3 Update

---
+++ Assets/_UnityStudy/2_R3/R3Test.cs
@@ -0,0 +1,57 @@
+using R3;
+using R3.Triggers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class R3Test : MonoBehaviour
+{
+    [SerializeField] private Text myText;
+
+    void Start()
+    {
+        SubjectTest();
+       
+    }
+
+    private void UpdateObservable()
+    {
+        Observable.
+            EveryUpdate(UnityFrameProvider.FixedUpdate, destroyCancellationToken).
+            Subscribe(_ =>
+            {
+                Debug.Log("Fixed Update");
+            });
+
+        this.UpdateAsObservable().
+            Subscribe(_ =>
+            {
+                Debug.Log("Update");
+            });
+    }
+
+    private void DoubleClick()
+    {
+        var clickStream = this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonDown(0));
+
+        clickStream.Chunk(clickStream.Debounce(TimeSpan.FromMilliseconds(300)))
+            .Where(x => x.Count() == 2)
+            .SubscribeToText(myText, x => $"Double Clicked! Click Count = {x.Count()}");
+    }
+
+    private void SubjectTest()
+    {
+        Subject<string> subject= new Subject<string>();
+
+        subject.Subscribe(msg => Debug.Log("Subscribe 1 " + msg));
+        subject.Subscribe(msg => Debug.Log("Subscribe 2 " + msg));
+        subject.Subscribe(msg => Debug.Log("Subscribe 3 " + msg));
+
+        subject.OnNext("Hello");
+        subject.OnNext("HI");
+    }
+}
