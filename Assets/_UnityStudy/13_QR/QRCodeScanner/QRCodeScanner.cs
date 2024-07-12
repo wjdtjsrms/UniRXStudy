fb3bb1e363d37d9fd5d37bfba09f6254f0d60555
wjdtjsrms <9kjmse@naver.com>
2024-07-12 13:09:17 +0900

    QR code Scanner 제작

--- Assets/_UnityStudy/13_QR/QRCodeScanner/QRCodeScanner.cs
+++ Assets/_UnityStudy/13_QR/QRCodeScanner/QRCodeScanner.cs
@@ -1,18 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
 
 public class QRCodeScanner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private RawImage rawImageBackground;
+
+    [SerializeField]
+    private AspectRatioFitter aspectRatioFitter;
+
+    [SerializeField]
+    private TextMeshProUGUI textOut;
+
+    [SerializeField]
+    private RectTransform scanZone;
+
+    private bool isCamAvailble;
+    private WebCamTexture cameraTexture;
+
     void Start()
     {
-        
+        SetUPCamera();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateCameraRender();
+    }
+
+    private void SetUPCamera()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            isCamAvailble = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].isFrontFacing == false)
+            {
+                cameraTexture = new WebCamTexture(devices[i].name, (int)scanZone.rect.width, (int)scanZone.rect.height);
+            }
+        }
+
+        cameraTexture?.Play();
+        rawImageBackground.texture = cameraTexture;
+        isCamAvailble = true;
+    }
+
+    private void UpdateCameraRender()
+    {
+        if (cameraTexture == null)
+            return;
+
+        float ratio = cameraTexture.width / cameraTexture.height;
+        aspectRatioFitter.aspectRatio = ratio;
+
+        int orientation = cameraTexture.videoRotationAngle;
+        rawImageBackground.rectTransform.localEulerAngles = new Vector3(0, 0, orientation);
+    }
+
+    public void OnClickScan()
+    {
+        Scan();
+    }
+
+    private void Scan()
+    {
+        try
+        {
+            IBarcodeReader barcodeReader = new BarcodeReader();
+            Result result = barcodeReader.Decode(cameraTexture.GetPixels32(), cameraTexture.width, cameraTexture.height);
+
+            if (result != null)
+            {
+                textOut.text = result.Text;
+            }
+            else
+            {
+                textOut.text = "Failed In Try";
+            }
+        }
+        catch
+        {
+            textOut.text = "Failed In Try";
+        }
     }
 }
